773661f03744771e393d8c2d6f8c7630e5483c65
stevenbai0724 <stevenbai0724@gmail.com>
2021-07-22 21:53:33 -0400

    7/22/21

---
+++ codeforcesCPPTemplate.cpp
@@ -0,0 +1,19 @@
+//
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long 
+
+signed main(){
+    cin.tie(nullptr)->sync_with_stdio(false);
+
+    int t; cin>>t;
+    while(t--){
+
+
+
+
+    }
+
+
+    return 0;
+}
