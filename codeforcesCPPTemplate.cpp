cf3950cd2b6c04909baa1a020f3c1f87912caac5
stevenbai0724 <stevenbai0724@gmail.com>
2021-08-17 21:53:12 -0400

    8/17/21

--- codeforcesCPPTemplate.cpp
+++ codeforcesCPPTemplate.cpp
@@ -2,15 +2,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define int long long 
-
+#define string std::string
 signed main(){
     cin.tie(nullptr)->sync_with_stdio(false);
 
     int t; cin>>t;
+
     while(t--){
-
-
-
+        
     }
 
 
